8713498a1a1dea33f3d0dbad06c64b12b51933aa
chrosslaw <Chrosslaw@gmail.com>
2022-05-18 12:04:27 -0500

    First save

---
+++ main.js
@@ -0,0 +1,53 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+  const dnaBases = ["A", "T", "C", "G"];
+  return dnaBases[Math.floor(Math.random() * 4)];
+};
+
+// Returns a random single stand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = [];
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase());
+  }
+  return newStrand;
+};
+
+const pAequorFactory = (number, array) => {
+  return {
+    _specimenNum: number,
+    _arr: array,
+    mutate() {
+      let newDna = [];
+      let baseArray = ["A", "T", "C", "G"];
+      let randomDnaBaseIndex = Math.floor(Math.random() * this._arr.length);
+      let mutated = this._arr[randomDnaBaseIndex];
+      baseArray.splice(baseArray.indexOf[mutated], 1);
+      this._arr.splice(
+        randomDnaBaseIndex,
+        1,
+        baseArray[Math.floor(Math.random() * 3)]
+      );
+      return this._arr;
+    },
+    compareDNA(obj) {
+      const dnaToCompare = obj;
+    },
+    // get name() {
+    //   return this._name;
+    // },
+    // get breed() {
+    //   return this._breed;
+    // },
+    // set name(name) {
+    //   this._name = name;
+    // }
+    // set weight(weight) {
+    //   this._weight = weight;
+    // },
+  };
+};
+
+const dnaTest = pAequorFactory(1, mockUpStrand());
+console.log(dnaTest);
+console.log(dnaTest.mutate());
