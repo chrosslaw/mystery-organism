b9a8ea774cace877ab5e528a5bc0a437c870c071
chrosslaw <Chrosslaw@gmail.com>
2022-05-18 14:43:57 -0500

    accept changes

--- main.js
+++ main.js
@@ -16,11 +16,11 @@ const mockUpStrand = () => {
 const pAequorFactory = (number, array) => {
   return {
     _specimenNum: number,
-    _arr: array,
+    _array: array,
     mutate() {
       let newDna = [];
       let baseArray = ["A", "T", "C", "G"];
-      let randomDnaBaseIndex = Math.floor(Math.random() * this._arr.length);
+      let randomDnaBaseIndex = Math.floor(Math.random() * this._array.length);
       let mutated = this._arr[randomDnaBaseIndex];
       baseArray.splice(baseArray.indexOf[mutated], 1);
       this._arr.splice(
@@ -28,14 +28,39 @@ const pAequorFactory = (number, array) => {
         1,
         baseArray[Math.floor(Math.random() * 3)]
       );
-      return this._arr;
+      return this._array;
     },
     compareDNA(obj) {
+      let comparedArray = [];
       const dnaToCompare = obj;
+      for (i = 0; i < this._array.length; i++) {
+        if (this._array[i] === dnaToCompare._array[i]) {
+          comparedArray.push(this._arr[i]);
+        }
+      }
+      console.log(comparedArray);
+      console.log(
+        `specimen #${this._specimenNum} and specimen #${
+          comparedArray._specimenNum
+        } have ${
+          (comparedArray.length / this._array.length) * 100
+        }% DNA in common`
+      );
+    },
+    willLikelySurvive() {
+      let count = 0;
+      for (let i of this._array) {
+        if (i === "C" || i === "G") {
+          count += 1;
+        }
+      }
+      return (count / this._array.length) * 100 >= 60 ? true : false;
     },
   };
 };
 
-const dnaTest = pAequorFactory(1, mockUpStrand());
-console.log(dnaTest);
-console.log(dnaTest.mutate());
+const specimen1 = pAequorFactory(1, mockUpStrand());
+const specimen2 = pAequorFactory(2, mockUpStrand());
+console.log(specimen1.willLikelySurvive());
+// console.log(specimen2);
+// console.log(specimen1.compareDNA(specimen2));
